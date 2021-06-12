feae995c4d574f235453c5ccfb44491c31377592
mehrad <mehrad.meraji@gmail.com>
2021-06-12 10:46:40 -0400

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import Container from '../components/container'
 import Layout from '../components/layout'
 import {getSiteDetails} from '../lib/api'
 import Head from 'next/head'
-import Header from "../components/header";
 import RenderSections from "../components/render-sections";
 import {useState} from "react";
 import PasswordPage from "./passwordPage";
-import Footer from "../components/footer";
 import "animate.css"
 import TitleCard from "./titleCard";
 
@@ -19,20 +16,14 @@ export default function Index({ siteSettings, preview }) {
       <Layout preview={preview}>
         <Head>
           <title>{`${siteName} - ${title}`}</title>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
-        <Container>
-          <Header siteName={siteName}/>
-          <div className="flex flex-1">
-            {isPasswordProtected && !authenticate ? <PasswordPage authenticate={setAuthenticate}/> : null}
-            {isPasswordProtected && authenticate ?  <>
-                { !loaded ? <TitleCard loaded={setLoaded}/> : null }
-              </> : null }
-            <RenderSections visible={isPasswordProtected ? authenticate : true} sections={content}/>
-          </div>
-          <Footer />
-        </Container>
+        <div className="flex items-center justify-center flex-wrap flex-grow">
+          {isPasswordProtected && !authenticate ? <PasswordPage authenticate={setAuthenticate}/> : null}
+          {isPasswordProtected && authenticate ?  <>
+              { !loaded ? <TitleCard loaded={setLoaded}/> : null }
+            </> : null }
+          <RenderSections visible={isPasswordProtected ? authenticate : true} sections={content}/>
+        </div>
       </Layout>
     </>
   )
